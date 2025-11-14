714ba9e260fd3991d20f8f6af5075629f7ea36ba
Jonathan150408 <jonathan.junod@eduvaud.ch>
2025-11-14 16:35:07 +0100

    feat(amusement-park) : terminer la fonction 1
    [20][In the work]

--- exercices/amusement-park/amusement-park.js
+++ exercices/amusement-park/amusement-park.js
@@ -10,7 +10,8 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  const visitor = {name, age, ticketId};
+  return visitor;
 }
 
 /**
@@ -20,7 +21,8 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Remove this line and implement the function');
+  visitor.ticketId = null;
+  return visitor;
 }
 
 /**
@@ -31,7 +33,12 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  
+
+  return ?;
+
+  }
+
 }
 
 /**
