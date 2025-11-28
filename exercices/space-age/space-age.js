5008f6735dc40a32d736c26104824615c4512779
Jonathan150408 <jonathan.junod@eduvaud.ch>
2025-11-28 13:41:56 +0100

    feat(space age) : terminer l'exercice
    [20][Done]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,36 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, time) => {
+  let multiplicator;
+  switch(planet){
+    case "mercury":
+      multiplicator = 0.2408467;
+      break;
+    case "venus":
+      multiplicator = 0.61519726;
+      break;
+    case "earth":
+      multiplicator = 1;
+      break;
+    case "mars":
+      multiplicator = 1.8808158;
+      break;
+    case "jupiter" :
+      multiplicator = 11.862615;
+      break;
+    case "saturn":
+      multiplicator = 29.447498;
+      break;
+    case "uranus":
+      multiplicator = 84.016846;
+      break;
+    case "neptune":
+      multiplicator = 164.79132;
+      break;
+    default:
+      throw new Error("not a planet");
+  }
+
+  return Math.round(time/31557600/multiplicator*100)/100;
 };
