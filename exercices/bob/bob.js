5ca3832e6ef2f9995dab83f69977bfd99ae35348
Jonathan150408 <jonathan.junod@eduvaud.ch>
2025-12-05 14:05:12 +0100

    feat(bob) : terminer l'exercice
    [60][Done]

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -4,13 +4,19 @@
 //
 
 export const hey = (message) => {
-  if (isUpperCase(message) && message.trim()[message.lenght - 1] == "?")
+  let upper = message.toUpperCase();
+  message = message.trim();
+  //test 1. upper case = basic string 2. finish with a '?' 3. Contains at least 1 letter
+  if (upper == message && message[message.length - 1] == "?" && /[A-Z]/.test(upper))
     return "Calm down, I know what I'm doing!";
-  else if (isUpperCase(message))
+  //test 1. upper case = basic string 2. Contains at least 1 letter
+  else if (upper == message && /[A-Z]/.test(upper))
     return "Whoa, chill out!";
-  else if (message.trim()[message.lenght - 1] == "?")
+  //test 1. finish with a '?'
+  else if (message[message.length - 1] == "?")
     return "Sure.";
-  else if (message.trim() == "")
+  //test 1. message is empty (= silence)
+  else if (message == "")
     return "Fine. Be that way!";
   else
     return "Whatever."
