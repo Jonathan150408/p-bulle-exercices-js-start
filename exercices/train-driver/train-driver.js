5c9bba8bb3331a15c67d58e4ef1d5020c8c4f2cb
Jonathan150408 <jonathan.junod@eduvaud.ch>
2025-12-12 16:09:02 +0100

    feat (train driver) : terminer l'exercice
    [90][Done]

--- exercices/train-driver/train-driver.js
+++ exercices/train-driver/train-driver.js
@@ -1,8 +1,4 @@
 // @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
 
 /**
  * Return each wagon's id in form of an array.
@@ -10,8 +6,8 @@
  * @param {...number} ids
  * @returns {number[]} wagon ids
  */
-export function getListOfWagons(a, b, c, d, e, f, g, h, i, j, k, l, m, n) {
-  throw new Error('Remove this line and implement the function');
+export function getListOfWagons(...ids) {
+  return ids;
 }
 
 /**
@@ -21,7 +17,8 @@ export function getListOfWagons(a, b, c, d, e, f, g, h, i, j, k, l, m, n) {
  * @returns {number[]} reordered list of wagons
  */
 export function fixListOfWagons(ids) {
-  throw new Error('Remove this line and implement the function');
+  const [first, second, ...rest] = ids
+  return [...rest, first, second]
 }
 
 /**
@@ -32,7 +29,9 @@ export function fixListOfWagons(ids) {
  * @returns {number[]} corrected list of wagons
  */
 export function correctListOfWagons(ids, missingWagons) {
-  throw new Error('Remove this line and implement the function');
+  const [locomotive, ...rest] = ids
+  const [...missing] = missingWagons
+  return [locomotive, ...missing, ...rest]
 }
 
 /**
@@ -43,7 +42,7 @@ export function correctListOfWagons(ids, missingWagons) {
  * @returns {Record<string, string>} extended route information
  */
 export function extendRouteInformation(information, additional) {
-  throw new Error('Remove this line and implement the function');
+  return {...information, ...additional}
 }
 
 /**
@@ -53,5 +52,6 @@ export function extendRouteInformation(information, additional) {
  * @returns {[string, Record<string, string>]} array with arrival time and object without arrival time
  */
 export function separateTimeOfArrival(information) {
-  throw new Error('Remove this line and implement the function');
+  const {timeOfArrival, ...info} = information
+  return [timeOfArrival, info]
 }
