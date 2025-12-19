5388ab57846e7a826d4d360f350f6817ded9c071
Jonathan150408 <jonathan.junod@eduvaud.ch>
2025-12-19 16:31:13 +0100

    Feat(pascals-triangle) : commencer l'exercice
    [30][In the work]

--- exercices/pascals-triangle/pascals-triangle.js
+++ exercices/pascals-triangle/pascals-triangle.js
@@ -1,8 +1,21 @@
-//
-// This is only a SKELETON file for the 'Pascals Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+//@ts-check
+/** 
+ * @param {number} numberofRows 
+ */
+export const rows = (numberofRows) => {
+  let result = []
 
-export const rows = () => {
-  throw new Error('Remove this line and implement the function');
+  for (let i = 0; i < numberofRows; i++){
+    //creates an empty row
+    result[i] = []
+    for (let j = 0; j < numberofRows; j++){
+      //first row
+      if (i == 0)
+        result[i][j] = 1
+      
+      result[i][j] = result[i - 1][j] + result[i][j];
+    }
+  }
+
+  return result
 };
