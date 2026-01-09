8c66713381451358fea9677e372c87c72b4f22aa
Jonathan Junod <jonathan.junod@eduvaud.ch>
2026-01-09 13:24:03 +0100

    feat(pascal's triangle) : terminer l'exercice
    [30][Done]

--- exercices/pascals-triangle/pascals-triangle.js
+++ exercices/pascals-triangle/pascals-triangle.js
@@ -3,17 +3,23 @@
  * @param {number} numberofRows 
  */
 export const rows = (numberofRows) => {
+  /**
+   * @type {number[][]}
+   */
   let result = []
 
   for (let i = 0; i < numberofRows; i++){
     //creates an empty row
     result[i] = []
-    for (let j = 0; j < numberofRows; j++){
-      //first row
-      if (i == 0)
+    for (let j = 0; j < i + 1; j++){
+      //every first and last value of each line
+      if (j == 0 || i == j){
         result[i][j] = 1
-      
-      result[i][j] = result[i - 1][j] + result[i][j];
+      }
+      //set the value to be the sum of the two above it
+      else{
+        result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+      }
     }
   }
 
