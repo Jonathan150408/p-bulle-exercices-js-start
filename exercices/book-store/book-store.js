80afdd06ed26a4f123c958b62498b416f99b415c
Jonathan Junod <jonathan.junod@eduvaud.ch>
2026-01-09 15:05:20 +0100

    chore(book store) : stagner sur l'exercice, aucune idée
    [10][In the work]

--- exercices/book-store/book-store.js
+++ exercices/book-store/book-store.js
@@ -1,8 +1,19 @@
-//
-// This is only a SKELETON file for the 'BookStore' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+//@ts-check
 
+/**
+ * Determines the price of the basket
+ *
+ * @param {number[]} books the basket of books
+ *
+ * @returns {number | undefined} the price with the biggest discount
+ */
 export const cost = (books) => {
-  throw new Error('Remove this line and implement the function');
+  let cost
+  let done = false
+
+  while (books.length > 2 && !done){
+    if (books.includes(1, 2, 3, 4, 5))
+  }
+
+  return cost
 };
