c8ee82618b7785efe412f9604dd733d6c93deb81
Jonathan Junod <jonathan.junod@eduvaud.ch>
2026-01-09 12:53:13 +0100

    fix(heads or tails) : rename the resolve function
    [10][Done]

--- exercices/heads-or-tails/heads-or-tails.js
+++ exercices/heads-or-tails/heads-or-tails.js
@@ -1,11 +1,11 @@
 //@ts-check
 export function headsOrTails() {
   //creates a new promise
-  return new Promise((successCallBack) => {
+  return new Promise((resolve) => {
     //waits 1000 miliseconds
     setTimeout(() => {
       //after 1 sec, returns true
-      successCallBack(true);
+      resolve(true);
     }, 1000);
   })
 }
